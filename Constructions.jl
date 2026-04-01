f526a8112ebd9d8c772e147945d654b9587e9638
jorqueraian <jorqueraian@gmail.com>
2026-04-01 14:49:38 -0600

    moved real etf to case O code to Cosntructions

--- Constructions.jl
+++ Constructions.jl
@@ -24,6 +24,29 @@ function maximal_case_O_etf(d::Int, p::Int)
     matrix(GF(p), gram + 3*I[1:n,1:n])
 end
 
+
+function real_etf_to_case_O(gram::Matrix{BigFloat}, d::Int, char::Int)::FqMatrix
+    n = size(gram)[1];
+    (n == size(gram)[2]) || throw(DomainError(size(gram),"gram must be square"));
+
+    ff = GF(char);
+    if n == 2*d
+        if !is_square(ff(n-1))
+            ff = GF(char, 2, "a");
+        end
+        a = sqrt(ff(n-1));
+        c = 2*a;
+    else
+        a = ff(Int(sqrt(d*(n-1)/(n-d))));
+        c = ff(Int((n/d)*sqrt(d*(n-1)/(n-d))));
+    end
+    
+    gram = (1/abs(gram[1,2]))*gram;
+    gram[diagind(gram)] .= 0;
+    matrix(ff, round.(Int, gram)) + diagonal_matrix(a,n,n)
+end
+
+
 function reconstruct_frame_from_gram(gram, case)
     # see Thm 3.13 and 3.15 of citation needed.
     n = size(gram)[1];
--- GameOfSloanesTools.jl
+++ GameOfSloanesTools.jl
@@ -13,24 +13,3 @@ function matrix_from_file(file_loc::String, d::Int, n::Int)::Union{Matrix{BigFlo
     end
 end
 
-function real_etf_to_case_O(gram::Matrix{BigFloat}, d::Int, char::Int)::FqMatrix
-    n = size(gram)[1];
-    (n == size(gram)[2]) || throw(DomainError(size(gram),"gram must be square"));
-
-    ff = GF(char);
-    if n == 2*d
-        if !is_square(ff(n-1))
-            ff = GF(char, 2, "a");
-        end
-        a = sqrt(ff(n-1));
-        c = 2*a;
-    else
-        a = ff(Int(sqrt(d*(n-1)/(n-d))));
-        c = ff(Int((n/d)*sqrt(d*(n-1)/(n-d))));
-    end
-    
-    gram = (1/abs(gram[1,2]))*gram;
-    gram[diagind(gram)] .= 0;
-    matrix(ff, round.(Int, gram)) + diagonal_matrix(a,n,n)
-end
-
